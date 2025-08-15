7096f6192dca207239b4b0806486f800c3e4b585
devRochaa <danielrocha.etec@gmail.com>
2025-08-15 17:07:20 -0300

    Database connection; Stock Controller: GetAll, GetById

---
+++ api/Models/Comment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int? StockId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        //Navigation Property
+        //para poder usar exemplo: Stock.Id
+        public Stock? Stock { get; set; }
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,29 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//sempre antes de builder
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
