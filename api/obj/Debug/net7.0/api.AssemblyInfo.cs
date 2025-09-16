47d594812831a33c04eff1f1a1c3c7d8c1700bb7
devRochaa <danielrocha.etec@gmail.com>
2025-09-15 21:42:37 -0300

    version 7.0 fix

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff2eacf821a016f1e8a8a2357dfdb09c7b9c6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23947c7b8c1ec163172dc66e2f7409e64d204177")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
